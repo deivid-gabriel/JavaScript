7a183c86497bf7a41acbc964bae48b53097488d9
DeividGabriel <66950466+deivid-gabriel@users.noreply.github.com>
2020-06-16 14:42:18 -0300

    Create Skills.js

---
+++ Rocketseat/Desafios/skills.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Desafio #3 da Rocketseat</title>
+</head>
+<body>
+  <script>
+    function habilidades() {
+      if (skills.includes('javascript', "ReactJS", "React Native")) {
+        console.log(true);
+      } else {
+        console.log(false);
+      }
+    }
+    var skills = ["Javascript", "ReactJS", "React Native"];
+    var resultados = habilidades();
+  </script>
+</body>
+</html>
